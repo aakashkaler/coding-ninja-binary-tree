f077ab3a2c300e857830af5e887ac4c443074e95
aakashkaler <68342702+aakashkaler@users.noreply.github.com>
2021-02-20 21:11:23 +0530

    Create removeleafnode.java

---
+++ removeleafnode.java
@@ -0,0 +1,46 @@
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+ */
+
+
+import java.util.*;
+
+public class Solution {
+
+	public static BinaryTreeNode<Integer> removeAllLeaves(BinaryTreeNode<Integer> root)
+    {
+        if(root==null)
+        {
+            return null;
+        }
+        else if(root.left==null&&root.right==null)
+        {
+            return null; 
+        }
+       
+        root.left=removeAllLeaves(root.left);
+        root.right=removeAllLeaves(root.right);
+        
+        return root;
+   
+            
+        
+	}
+
+
+
+}
